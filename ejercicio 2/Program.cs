31054182c33f2cff23c5d9789f0f1dc37f40429c
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 11:27:29 -0600

    1127

--- ejercicio 2/Program.cs
+++ ejercicio 2/Program.cs
@@ -6,7 +6,28 @@ namespace ejercicio_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("ingrese un numero");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            { 
+            case 1:
+                Console.WriteLine("eligio la opcion {0}", option);
+              break;
+            case 2:
+                Console.WriteLine("eligio la opcion {0}", option);
+            break;
+            case 3:
+                Console.WriteLine("eligio la opcion{0}", option);
+            break;
+            case 4:
+                Console.WriteLine ("eligio la opcion {0}", option):
+            break;
+             case 5:
+                Console.WriteLine("eligio la opcion {0}", option);
+            break;
+            default :
+                Console.WriteLine("esta opcion no existe");
+            break;
+
     }
 }
